abd6bbf0af4a2f66534ba2b5aa489f9fd6798f22
Rochita <69753947+rochita-sharma@users.noreply.github.com>
2021-11-30 21:51:14 +0530

    SE Project needs changes

---
+++ Blood_Bank_SE/Script.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    var w = window.innerWidth;
+
+    if(w > 767){
+        $('#menu-jk').scrollToFixed();
+    }else{
+        $('#menu-jk').scrollToFixed();
+    }
+    const galleeryLightbox = GLightbox({
+        selector: '.galleery-lightbox'
+      });
+})
+$("#search").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#table tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
